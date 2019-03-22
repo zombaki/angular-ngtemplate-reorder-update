193f7e2365851fb1fd6cc119ca72e9b9e23d8672
zombaki <piyush.nath.2272@gmail.com>
2019-03-22 10:06:56 -0500

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -23,7 +23,8 @@ export class HelloComponent {
     console.log(dragDrop);
     console.log(this.div);
     let a=dragDrop.createDrag(this.div);
-    console.log(dropREf)
     a._withDropContainer(dropREf);
+    console.log(dropREf);
+    console.log(a);
   }
 }
