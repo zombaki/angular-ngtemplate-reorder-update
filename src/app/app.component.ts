4b99d4c17d9bdd85920fbf99f3daeda06ff38edf
zombaki <piyush.nath.2272@gmail.com>
2019-03-22 09:48:40 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ViewChild, TemplateRef, ViewContainerRef ,ViewEncapsulation,ComponentFactoryResolver} from '@angular/core';
 import {MatAccordion} from '@angular/material';
 import {HelloComponent} from './hello.component';
+import { DragDrop } from '@angular/cdk/drag-drop';
 let counter = 1;
 @Component({
   selector: 'my-app',
@@ -14,15 +15,20 @@ export class AppComponent  {
   @ViewChild("piechartsContainer", {read: ViewContainerRef}) pieChartContainer: ViewContainerRef;
   @ViewChild(MatAccordion) accordion: MatAccordion;
 
-  constructor(private resolver: ComponentFactoryResolver) {}
+  constructor(private resolver: ComponentFactoryResolver,
+  private dragDrop:DragDrop) {}
 
   add() {
     let componentFactory = this.resolver.resolveComponentFactory(HelloComponent);
     let componentRef = componentFactory.create(this.pieChartContainer.injector);
     componentRef.instance.title = 'Dynamic accordion header' + counter++;
     this.pieChartContainer.createEmbeddedView(componentRef.instance.template);
+    //this.dragDrop.createDropList();
+    componentRef.instance.dragEnable(this.dragDrop);
+
     componentRef.changeDetectorRef.detectChanges();
-    componentRef.instance.panel.accordion = this.accordion;
+    //componentRef.instance.panel.accordion = this.accordion;
+    
   }
   dropLocal(){
     console.log('inside dropLcaol.');
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, TemplateRef, ViewContainerRef ,ViewEncapsulation} from '@angular/core';
+import { Component, Input, ViewChild, TemplateRef, ViewContainerRef ,ViewEncapsulation, ElementRef} from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import {MatExpansionPanel} from '@angular/material';
 
@@ -6,9 +6,11 @@ import {MatExpansionPanel} from '@angular/material';
   selector: 'hello',
   template: `
     <ng-template #tmpl>
-      <mat-expansion-panel cdkDrag>
+      <mat-expansion-panel>
+          <div #dvDragElement>
         <mat-expansion-panel-header>{{title}}</mat-expansion-panel-header>
         <p>Some text</p>
+        </div>
       </mat-expansion-panel>
     </ng-template>
   `,
@@ -17,7 +19,12 @@ import {MatExpansionPanel} from '@angular/material';
 })
 export class HelloComponent {
    @ViewChild('tmpl') template: TemplateRef<any>;
+   @ViewChild('dvDragElement') div:ElementRef;
   @ViewChild(MatExpansionPanel) panel: MatExpansionPanel;
   title: string;
-
+  dragEnable(dragDrop){
+    console.log(dragDrop);
+    console.log(this.div);
+    dragDrop.createDrag(this.div);
+  }
 }
